278e9ee1383f011ced6d77c64f2a83f065878db3
Henrique Douglas <henriquedouglas2803@gmail.com>
2026-02-10 20:01:15 -0300

    adiciona tela de login

--- projeto_login/script.js
+++ projeto_login/script.js
@@ -1,1 +1,16 @@
 console.log("Funcionando");
+
+/*
+ 1 - Pegar o valor do email e armazena em uma variavel
+ 2 - Pegar o valor da senha e armazena em uma variavel
+ 3 - Fazer condicao(IF) para verificar se email é igual = senai@gmail.com
+ e senha igual = senai
+ CASO a condicao seja verdadeira: 
+    redireciona o usuario para tela de home.html
+ CASO a condicao seja falsa:
+    exibir tela um alert() dizendo crendenciais incorreta
+*/
+
+function realizarLogin() {
+  window.location.href = "home.html";
+}
